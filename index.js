bf2a6641d4db7dfe5f0d049e7217e725721ef662
Quan Nguyen <ntquan87@gmail.com>
2023-10-05 17:12:14 +0700

    udpate get /hello

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World 123!</h1>')
+    res.send('<h1>Hello World 123455!</h1>')
 })
 
 app.get('/about', (req, res) => {
